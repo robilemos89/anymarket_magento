9d77e1d60f434a3d72d375e06abdd3d5c2914e42
Jose Eduardo - NPC <novapc@novapc.com.br>
2015-11-13 09:51:45 -0200

    Small Commit

--- app/code/community/DB1/AnyMarket/Helper/Product.php
+++ app/code/community/DB1/AnyMarket/Helper/Product.php
@@ -223,7 +223,7 @@ class DB1_AnyMarket_Helper_Product extends DB1_AnyMarket_Helper_Data
 
 	//ENVIA PRODUTO PARA O ANYMARKET
     public function sendProductToAnyMarket($idProduct){
-        //obter configuracoes - MultLoja
+        //obter configuracoes - Util MultLoja
         $product =  Mage::getModel('catalog/product')->load($idProduct);
         //$storeIds = $_product->getStoreIds();
         //foreach ($storeIds as $store) {
