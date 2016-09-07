fbcdef4b6fcd0720dd98c60c3294de241fc58a66
Jose Eduardo de Almeida Junior <joeduardo_almeidajr@hotmail.com>
2016-09-06 22:52:55 -0300

    [ANYMARKET] - SYNC ADDED

--- app/code/community/DB1/AnyMarket/Helper/Data.php
+++ app/code/community/DB1/AnyMarket/Helper/Data.php
@@ -216,9 +216,9 @@ class DB1_AnyMarket_Helper_Data extends Mage_Core_Helper_Abstract
         $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
         if ( $status == 200 || $status == 204 || $status == 201 ) {
-    			$retorno = array("error" => "0", "json" => $data_string, "return" => json_decode($curl_response));
-    		}elseif ( $status == 404 || $status == 500 || $status == 503 ) {
-    			$retorno = array("error" => "1", "json" => $data_string, "return" => json_decode($curl_response));
+    			$retorno = array("error" => "0", "json" => $data_string, "return" => json_encode($curl_response));
+    	}elseif ( $status == 404 || $status == 500 || $status == 503 ) {
+    			$retorno = array("error" => "1", "json" => $data_string, "return" => json_encode($curl_response));
         }else{
           if($err){
               $retorno = array("error" => "1", "json" => $data_string, "return" => 'Error Curl: '.$err );
@@ -244,7 +244,7 @@ class DB1_AnyMarket_Helper_Data extends Mage_Core_Helper_Abstract
               }
 
               if($retString != ''){
-                  $retorno = array("error" => "1", "json" => $data_string, "return" => json_decode($retString) );
+                  $retorno = array("error" => "1", "json" => $data_string, "return" => json_encode($retString) );
               }else{
                   $retorno = array("error" => "1", "json" => $data_string, "return" => utf8_encode($curl_response) );
               }
